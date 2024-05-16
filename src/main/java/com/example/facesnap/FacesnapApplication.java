5ed14f27bfd9794dc61da23ad03475b1875ad58c
gaston <gastonadkins009@gmail.com>
2024-05-16 12:10:22 +0200

    first commit

---
+++ src/main/java/com/example/facesnap/FacesnapApplication.java
@@ -0,0 +1,13 @@
+package com.example.facesnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacesnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacesnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/facesnap/FacesnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facesnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacesnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
